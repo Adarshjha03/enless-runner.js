6179ccf0a0b5d1168cc8c7f5efc017c3ed338521
Adarshjha03 <jha.adarshk03@gmail.com>
2023-09-05 11:50:09 +0530

    first comment

--- app.js
+++ app.js
@@ -1,11 +1,11 @@
 score=0;
 cross=true;
-
-// const audio=new Audio('music.mp3');
-// const gameover=new Audio('gameover.mp3');
-// setTimeout(()=>{
-// audio.play();
-// },1000);
+const chime = new Audio('chime.flac'); 
+const audio=new Audio('music.mp3');
+const gameover=new Audio('gameover.mp3');
+setTimeout(()=>{
+audio.play();
+},1000);
 
 
 window.addEventListener('keydown' , checkkey) ;
@@ -39,17 +39,32 @@ setInterval(() => {
     offsetX=Math.abs(dx-ox);
     offsetY=Math.abs(dy-oy);
 
-    if(offsetX<113&&offsetY<52){
-        gameOver.innerHTML= "Game Over - Reload to start over"
-        obstacle.classList.remove('obstacleAni');
-        // gameover.play();
-        // setTimeout(()=>{
-        //    gameover.pause();
-        //    audio.pause();
-        // },1000);
+    if (offsetX < 113 && offsetY < 52) {
+    if (dx > ox) {
+        // Check if the dino is behind the obstacle by comparing their bottom positions
+        const dinoBottom = dy + 134; // Assuming 134 is the height of the dino
+        const obstacleBottom = oy + 123; // Assuming 123 is the height of the obstacle
+
+        if (dinoBottom <= obstacleBottom) {
+            // The dino is behind or level with the obstacle, no game over
+            return;
+        }
     }
+
+    // If the conditions above are not met, it's a valid collision
+    gameOver.innerHTML = "Game Over - Reload to start over";
+    obstacle.classList.remove('obstacleAni');
+    gameover.play();
+    setTimeout(() => {
+        gameover.pause();
+        audio.pause();
+    }, 1000);
+}
+
     else if(offsetX<145&&cross){
+        
         score+=1;
+        chime.play();
         updateScore(score);
         cross=false;
         setTimeout(()=>{
